f43fd65d72a7b86d1f245b1db48c03581c96b5f7
Ahmed Hafsoh <Hafsohoyiza@gmail.com>
2022-09-15 18:36:42 -0700

    8-print_square.c

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -6,7 +6,7 @@
  */
 void print_square(int size)
 {
-	if (size <= 0)
+	if (size > 0)
 	{
 		_putchar('\n');
 	}
@@ -18,7 +18,7 @@ void print_square(int size)
 		{
 			for(j = 0; j < size; j++)
 			{
-				_putchar('#');
+				_putchar(35);
 			}
 			_putchar('\n');
 		}
