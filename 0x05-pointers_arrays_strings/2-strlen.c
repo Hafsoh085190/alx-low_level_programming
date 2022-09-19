53bc6a82343d0bd193e9221a323ebe79fc8e8031
Ahmed Hafsoh <Hafsohoyiza@gmail.com>
2022-09-19 10:41:25 -0700

    2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -4,12 +4,14 @@
  * _strlen - return the length of a given string
  * @s: char type pointer
  * @s: char type pointer
+ * Return: length of string
  */
 int _strlen(char *s)
 {
 	int len;
 
-	for (len = 0; s[len] != '\0'; len++);
+	for (len = 0; s[len] != '\0'; len++)
+		;
 
 	return (len);
 }
